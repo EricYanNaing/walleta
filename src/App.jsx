4332554cfefef9b2423aa98e477067abec6ede69
ericthedev7 <ericthedev97@gmail.com>
2025-10-31 14:14:58 +0700

    fixed @

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
 // App.jsx
-import './App.css';
-import { useLocation } from 'react-router-dom';
-import AppRoutes from './routes/AppRoutes';
-import Header from '../src/components/Header.jsx';
-import Footer from '../src/components/Footer.jsx';
-import useAuthStore from '../src/store/useAuthStore';
+import "./App.css";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import AppRoutes from "./routes/AppRoutes";
+import Header from "../src/components/Header.jsx";
+import Footer from "../src/components/Footer.jsx";
+import useAuthStore from "../src/store/useAuthStore";
 
 function App() {
   const token = useAuthStore((s) => s.token);
   const location = useLocation();
 
   // Hide layout on public auth pages or when not logged in
-  const authPages = ['/login', '/register', '/forgot-password'];
+  const authPages = ["/login", "/register", "/forgot-password"];
   const hideLayout = !token || authPages.includes(location.pathname);
 
   return (
     <section className="App">
       {!hideLayout && <Header />}
-      <AppRoutes />
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
       {!hideLayout && <Footer />}
     </section>
   );
